619e4685dded6bc5dce22b04c833c64556f3b40b
Defnull <define.null@gmail.com>
2013-03-12 18:12:57 +0400

    Getopt added, file names in msgs header while viewing several files

--- src/error_log_tool.erl
+++ src/error_log_tool.erl
@@ -7,79 +7,179 @@
 %% Exported Functions
 %%
 -export([main/1]).
--export([cat/1, cat/2, visit_log/2, filter/2, print_log/3, print_zlist/3]).
+-export([cat/1, cat/2, visit_log/2, filter/2, print_log/2, print_zlist/2]).
 -export([worker_fun/4]).
 
 -define(REPORT_SEPARATOR,"~n### ~p -----------------------------------------------------------------------~n").
-
+-define(FILE_SEPARATOR(File),"~n### ~p " ++ lists:flatten(io_lib:format("### ~100..-s ###", [File])) ++ "~n").
+-define(OPTS, [
+               {sasl,         undefined, "sasl",       undefined, "Only sasl reports"},
+               {'sasl-error', undefined, "sasl-error", undefined, "Only sasl error reports"},
+               {error,        undefined, "error",      undefined, "All standard or sasl error reports"},
+               {warn,         undefined, "warn",       undefined, "All standard or sasl error or warning reports"}
+              ]).
+        
 main([_|_]=Args) ->
-    [LogFilename|Options]=lists:reverse(Args),
-    try error_log_tool:cat([ list_to_atom(Opt) || [$-,$-|Opt] <- Options],LogFilename)
+    {ok, {Options, Files}} = getopt:parse(?OPTS, Args),
+
+    Format = case length(Files) of
+                 1 -> {format, plain};
+                 _ -> {format, with_filename}
+             end,
+    
+    try cat_logs(Files, [Format | Options])
     catch
         _:Reason ->
             io:format(standard_error,"error: ~p~n",[Reason]),
             halt(1)
     end;
 main(_) ->
-    usage().
-
-usage() ->
-    io:format(standard_error,"usage: error_log_cat OPTIONS LogFilename~n~n",[]),
-    io:format(standard_error,"OPTIONS~n",[]),
-    io:format(standard_error,"\t--sasl          - only sasl reports~n",[]),
-    io:format(standard_error,"\t--sasl-error    - only sasl error reports~n",[]),
-    io:format(standard_error,"\t--error         - all standard or sasl error reports~n",[]),
-    io:format(standard_error,"\t--warn          - all standard or sasl error or warning reports~n",[]),
+    getopt:usage(?OPTS, "error-log-tool"),
     halt(1).
 
 %%
 %% API Functions
 %%
 
+-record(print, {format}).
+
+cat_logs([Log | Tail], Opts) ->
+    cat(Opts, Log),
+    cat_logs(Tail, Opts);
+cat_logs([], _) ->
+    ok.
+
 cat(LogFilename) ->
     cat([], LogFilename).
 
 cat(Options, LogFilename) ->
-    print_log(Options,
-              fun(IoList) ->
-                      io:format(standard_io, "~s",[IoList])
-              end, 
-              LogFilename).
+    print_log([{file, LogFilename} | Options], LogFilename).
 
-print_log(Options, PrintFun, LogFilename) when is_function(PrintFun, 1) ->
-    visit_log(fun(LogZList)-> print_zlist(Options, PrintFun, LogZList) end, LogFilename).
+print_log(Options, LogFilename) ->
+    visit_log(fun(LogZList)-> print_zlist(Options, LogZList) end, LogFilename).
 
-print_zlist(Options, PrintFun, LogZList) when is_function(PrintFun, 1) ->
+print_zlist(Options, LogZList) ->
     LogZList1 = zlists:ziph(zlists:seq(1, 1000000000000, 1), LogZList),
 
     BunchedList = bunch_zlist(50, LogZList1),
     Self = self(),
     Reader = spawn(fun() ->
-                           print_work(1, PrintFun, undefined),
-                           Self ! ok end),
+                           print_work(1, undefined, Options),
+                           Self ! ok
+                   end),
     
     _WorkMan= spawn(fun() -> worker_manager(Reader,
-                                           erlang:system_info(logical_processors),
-                                           BunchedList,
-                                           Options) end),
+                                            erlang:system_info(logical_processors),
+                                            BunchedList,
+                                            Options)
+                    end),
     receive
         ok ->
             ok
     end.
     
-print_evt(PrintFun, {N, Report}) ->
-    file:write(standard_io, io_lib:format(?REPORT_SEPARATOR, [N])),
+print_evt({N, Report}, Format) ->
+    file:write(standard_io, io_lib:format(Format, [N])),
     file:write(standard_io, Report),
     ok.
 
 visit_log(Fun, LogFilename) when is_function(Fun, 1) ->
     {ok,Log}=disk_log:open([{name,LogFilename}, {file,LogFilename}, {mode,read_only}]),
-    try Z=zlists_disk_log:read(Log),
+    try Z = zlists_disk_log:read(Log),
          Fun(Z)
     after
         disk_log:close(Log)
     end.
 
+%%
+%% Local Functions
+%%
+print_work(1, undefined, Options)
+  when is_list(Options) ->
+    File = proplists:get_value(file, Options),
+    Format = case proplists:get_value(format, Options) of
+                 plain ->
+                     ?REPORT_SEPARATOR;
+                 with_filename ->
+                     ?FILE_SEPARATOR(File)
+             end,
+    
+    print_work(1, undefined, #print{format = Format});
+
+print_work(Num, Num, #print{}) ->
+    ok;
+
+print_work(Num, undefined, #print{format = Format}=Print) ->
+    receive
+        {Pid, Num, Work} ->
+            Pid ! ok,
+            [ print_evt({N, W}, Format) || {N,W} <- Work ],
+            print_work(Num + 1, undefined, Print);
+        {max, Num} ->
+            ok;
+        {max, Last} ->
+            print_work(Num, Last, Print)
+    end;
+
+print_work(Num, Last, #print{format = Format}=Print)
+  when is_integer(Last) ->
+    receive
+        {Pid, Num, Work} ->
+            Pid ! ok,
+            [ print_evt({N, W}, Format) || {N,W} <- Work ],
+            print_work(Num + 1, Last, Print)
+    end.            
+    
+%%------------------------------------------------------------------------------
+
+worker_manager(Reader, ProcNum, Zlist, Options) ->
+    process_flag(trap_exit, true),
+    
+    {Tasks, Later} = zlists:scroll(ProcNum, Zlist),
+    lists:foldl(fun(Task, Acc) ->
+                        spawn_link(fun() ->
+                                           worker_fun(Reader, Acc, Task, Options)
+                                   end),
+                        Acc+1
+               end, 1, Tasks),
+    
+    worker_manager_i(Reader, ProcNum, Later, Options).
+
+worker_manager_i(Reader, Last, [], _) ->
+    Reader ! {max, Last};
+
+worker_manager_i(Reader, Last, [Bunch | Zlist], Opts) ->
+    receive
+        {'EXIT', _, _} ->
+            ok
+    end,
+    spawn_link(fun() -> worker_fun(Reader, Last + 1, Bunch, Opts) end),
+    worker_manager_i(Reader, Last + 1, Zlist(), Opts).
+
+worker_fun(Reader, Num, Task, Options) ->
+    Result = worker(Task, Options),
+    Reader ! {self(), Num, Result},
+    receive
+        ok ->
+            ok
+    end.
+
+worker(List, Options) ->
+    lists:map( fun({_N,{NTime,Evt}})->
+                       Event = {calendar:now_to_universal_time(NTime),Evt},
+                       {_N, error_log_tool_fmt:format_event(Event)}
+               end, filter(Options, List)).
+
+%%------------------------------------------------------------------------------
+
+bunch_zlist(Num, Zlist) ->
+    case zlists:scroll(Num, Zlist) of
+        {[], _} ->
+            [];
+        {L, T} ->
+            [L | fun() -> bunch_zlist(Num, T) end]
+    end.
+                 
 filter([]=_Options,LogZList) ->
     LogZList;
 filter([Opt|Tail]=_Options,LogZList) ->
@@ -128,78 +228,3 @@ filter([Opt|Tail]=_Options,LogZList) ->
             Z=LogZList
     end,
     filter(Tail,Z).
-
-%%
-%% Local Functions
-%%
-print_work(Num, _, Num) ->
-    ok;
-
-print_work(Num, PrintFun, undefined) ->
-    receive
-        {Pid, Num, Work} ->
-            Pid ! ok,
-            [ print_evt(PrintFun, {N, W}) || {N,W} <- Work ],
-            print_work(Num + 1, PrintFun, undefined);
-        {max, Num} ->
-            ok;
-        {max, Last} ->
-            print_work(Num, PrintFun, Last)
-    end;
-
-print_work(Num, PrintFun, Last) ->
-    receive
-        {Pid, Num, Work} ->
-            Pid ! ok,
-            [ print_evt(PrintFun, {N, W}) || {N,W} <- Work ],
-            print_work(Num + 1, PrintFun, Last)
-    end.            
-    
-
-worker(List, Options) ->
-    lists:map( fun({_N,{NTime,Evt}})->
-                       Event = {calendar:now_to_universal_time(NTime),Evt},
-                       {_N, error_log_tool_fmt:format_event(Event)}
-               end, filter(Options, List)).
-
-%%------------------------------------------------------------------------------
-
-worker_manager(Reader, ProcNum, Zlist, Options) ->
-    process_flag(trap_exit, true),
-    
-    {Tasks, Later} = zlists:scroll(ProcNum, Zlist),
-    lists:foldl(fun(Task, Acc) ->
-                        spawn_link(fun() -> worker_fun(Reader, Acc, Task, Options) end),
-                        Acc+1
-               end, 1, Tasks),
-   
-    worker_manager_i(Reader, ProcNum, Later, Options).
-
-worker_manager_i(Reader, Last, [], _) ->
-    Reader ! {max, Last};
-
-worker_manager_i(Reader, Last, [Bunch | Zlist], Opts) ->
-    receive
-        {'EXIT', _, _} ->
-            ok
-    end,
-    spawn_link(fun() -> worker_fun(Reader, Last + 1, Bunch, Opts) end),
-    worker_manager_i(Reader, Last + 1, Zlist(), Opts).
-
-worker_fun(Reader, Num, Task, Options) ->
-    Reader ! {self(), Num, worker(Task, Options)},
-    receive
-        ok ->
-            ok
-    end.
-
-%%------------------------------------------------------------------------------
-
-bunch_zlist(Num, Zlist) ->
-    case zlists:scroll(Num, Zlist) of
-        {[], _} ->
-            [];
-        {L, T} ->
-            [L | fun() -> bunch_zlist(Num, T) end]
-    end.
-                 
